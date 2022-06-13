98c9ee1857a8185d6188b6db6ee612e5d0cb0a72
Lucas Gerlach Nachtigall <lucasnachtigall@hotmail.com>
2022-06-13 17:25:24 -0300

    Tips

--- AngularTypeV0.0.1/src/app/app-routing.module.ts
+++ AngularTypeV0.0.1/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', loadChildren: () => import('src/app/home/home.module').then(module => module.HomeModule)},
+  {{ path: 'settings', loadChildren: () => import('src/app/settings/settings.module').then(module => module.SettingsModule) },},
+  {path: '', loadChildren: () => import('src/app/utils/utils.module').then(module => module.UtilsModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- AngularTypeV0.0.1/src/app/home/home.module.ts
+++ AngularTypeV0.0.1/src/app/home/home.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home.component';
 
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
 
 
 @NgModule({
@@ -9,7 +10,8 @@ import { HomeComponent } from './home.component';
     HomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    HomeRoutingModule
   ]
 })
 export class HomeModule { }
